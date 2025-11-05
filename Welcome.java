6b6bd9aaba5c61a14699e5dad59b9060ef69df1e
giljoo93 <dlrlfwn3000@gmail.com>
2025-11-05 15:39:07 +0900

    make menu text

--- Welcome.java
+++ Welcome.java
@@ -1,5 +1,11 @@
 public class Welcome {
     public static void main(String[] args) {
+        System.out.println("============================");
         System.out.println("Welcome to the Choon_JAVA!");
+        System.out.println("----------------------------");
+        System.out.println("Please select a menu number.");
+        System.out.println("============================");
+        System.out.println("(0)Login (1)Sign up (2)exit");
+
     }
 }
