ba84e523937e2d062aa1c74c7a1a8a730aff5360
giljoo93 <dlrlfwn3000@gmail.com>
2025-11-11 16:24:37 +0900

    백준 관련 파일 병합

---
+++ BAEKJOON/_01class/_01.java
@@ -0,0 +1,10 @@
+package BAEKJOON._01class;
+
+
+public class _01 {
+	public static void main (String[] args) {
+    
+    System.out.print("Hello World!");
+        
+    }
+}
---
+++ test.java
@@ -0,0 +1,21 @@
+public class test {
+    public static void main(String[] args) {
+        
+
+        int i;
+
+        for(i=0; i<5; i++){
+            System.out.println("첫 i 루프 반복 +" +i);
+        }
+
+        for(i=0; i<5; i++){
+            System.out.println("둘 i 루프 반복 +" +i);
+        }
+
+
+
+
+
+
+    }
+}
